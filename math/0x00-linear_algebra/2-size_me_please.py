f42db5b5bec794540ab4acb28f97a7ff9beb429d
Daniel Quintero <dgquintero02@hotmail.com>
2020-07-13 15:34:25 -0500

    adding 3-flip

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import numpy as np
+
+def matrix_shape(matrix):
+    matrix_shape = np.shape(matrix)
+    return (matrix_shape)
---
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import numpy as np
+
+def matrix_transpose(matrix):
+    matrix_T = np.transpose(matrix)
+    return matrix_T
