411dc7a0e4e6ec3d5ddf003da703e46da5a5c68c
Daniel Quintero <dgquintero02@hotmail.com>
2020-07-13 17:32:58 -0500

    adding ans 5 and 6

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
 import numpy as np
+
+def add_matrices2D(mat1, mat2):
+    if np.shape(mat1) == np.shape(mat2):
+        sum_array = np.add(mat1, mat2)
+        return sum_array
+
+    return None
---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import numpy as np
+
+def cat_arrays(arr1, arr2):
+    new_array = arr1 + arr2
+    return new_array
