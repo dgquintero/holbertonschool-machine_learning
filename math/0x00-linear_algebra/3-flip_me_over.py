a15dffd9635c4e05d929ab082e0fe74400a41f2f
Daniel Quintero <dgquintero02@hotmail.com>
2020-07-15 15:59:20 -0500

    3 fixed

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-# function that transpose a matrix
+"""function that transpose a matrix"""
 
 
 def matrix_transpose(matrix):
-    # matrix transpose function
+    """matrix transpose function"""
     r = [[0, 0, 0], [0, 0, 0]]
     m = matrix
     r = [[m[j][i] for j in range(len(m))]for i in range(len(m[0]))]
