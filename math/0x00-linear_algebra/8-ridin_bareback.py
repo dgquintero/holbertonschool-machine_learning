97cb04f7a6ea79e6d884c34fcbe296d7f9631919
Daniel Quintero <dgquintero02@hotmail.com>
2020-07-14 23:12:31 -0500

    project update

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -4,7 +4,14 @@ import numpy as np
 
 def mat_mul(mat1, mat2):
     try:
-        new_array = np.dot(mat1, mat2)
-        return new_array
+        #new_array = np.dot(mat1, mat2)
+        result = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        # iterate through rows 
+        for i in range(len(mat1)):
+            # iterate through columns
+            for j in range(len(mat1[0])):
+                result[i][j] = mat1[i][j] * mat2[j][i]
+        return result
+        #return new_array
     except:
         return None
