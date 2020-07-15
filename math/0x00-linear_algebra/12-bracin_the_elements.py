15b06c5afa2f79d6f1fb01d65f4697bd29bb94a5
Daniel Quintero <dgquintero02@hotmail.com>
2020-07-15 16:47:30 -0500

    given permissions to 12 files

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -4,5 +4,5 @@ import numpy as np
 
 
 def np_elementwise(mat1, mat2):
-    """function elementwise""" 
+    """function elementwise"""
     return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
