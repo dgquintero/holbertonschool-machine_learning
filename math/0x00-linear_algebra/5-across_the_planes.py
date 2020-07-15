b13f7a64c811f142a83504d31fdad30e74859a65
Daniel Quintero <dgquintero02@hotmail.com>
2020-07-15 16:07:22 -0500

    adding function that add two matrices

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
-import numpy as np
+"""function add two matrices"""
 
 
 def add_matrices2D(mat1, mat2):
-    if np.shape(mat1) == np.shape(mat2):
+    """function add two matrices"""
+    mat1m = len(mat1)
+    mat1n = len(mat1[0])
+    mat2m = len(mat2)
+    mat2n = len(mat2[0])
+    if mat1m == mat2m and mat1n == mat2n:
         result = [[0, 0], [0, 0]]
         # iterate through rows
         for i in range(len(mat1)):
