7f62fd3809f9db342a675914a719123fb32f04a8
Daniel Quintero <dgquintero02@hotmail.com>
2020-07-15 12:00:14 -0500

    fixing 7 answer

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
-import numpy as np
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
-    new_array = mat1
-    print(axis)
-    if axis == 0:
-        # new_array.append(mat2[0])
-        new_array = mat1 + mat2
-    elif axis == 1:
+    # a copy
+    a = [row[:] for row in mat1]
+    b = [row[:] for row in mat2]
+    # know what is the size of the columns and rows
+    mat1m = len(mat1)
+    mat1n = len(mat1[0])
+    mat2m = len(mat2)
+    mat2n = len(mat2[0])
+
+    new_array = []
+    if (axis == 0) and (mat1n == mat1n):
+        new_array = a + b
+    elif (axis == 1) and (mat1m == mat2m):
         for i in range(len(mat1[0])):
-            new_array[i].append(mat2[i][0])
-    # new_array = np.concatenate((mat1, mat2), axis)
+            new_array.append(a[i] + b[i])
+    else:
+        return None
     return new_array
