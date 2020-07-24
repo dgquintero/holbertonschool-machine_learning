dde5dcea4fa3a6a12b9a41079b40836602050199
Daniel Quintero <dgquintero02@hotmail.com>
2020-07-24 10:26:55 -0500

    9 fixed

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+"""function def summation_i_squared"""
 
 
 def summation_i_squared(n):
+    """function def summation_i_squared"""
     if type(n) == int:
         sum = 0
         for i in range(n + 1):
