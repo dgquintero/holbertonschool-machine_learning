09dda90bfb68b57ea6fb615423416eb18899ce4d
Daniel Quintero <dgquintero02@hotmail.com>
2020-07-24 12:50:40 -0500

    9 fixed

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -7,6 +7,6 @@ def summation_i_squared(n):
     if (n == 0):
         return 0
     elif type(n) is int:
-        return (int(n ** 2) + summation_i_squared(n - 1))
+        return (int((n ** 2) + summation_i_squared(n - 1)))
     else:
         None
