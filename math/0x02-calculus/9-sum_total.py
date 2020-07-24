600e8a85ce13f26d7e14601952ccb06fa01d8e2a
Daniel Quintero <dgquintero02@hotmail.com>
2020-07-24 14:42:39 -0500

    adding 9 fixed

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,10 +4,16 @@
 
 def summation_i_squared(n):
     """function def summation_i_squared"""
-    if (n == 0):
-        return 0
-    #elif type(n) is int:
-    elif isinstance(n, (int, float)) or n == int(n) or n > 1:
-        return (int((n ** 2) + summation_i_squared(n - 1)))
+    # if (n == 0):
+    #    return 0
+    # elif type(n) is int:
+    # elif isinstance(n, (int, float)) or n == int(n) or n > 1:
+    #    return (int((n ** 2) + summation_i_squared(n - 1)))
+    # else:
+    #    None
+    if type(n) is not int:
+        return None
+    elif n < 1:
+        return None
     else:
-        None
+        return int((n * (1 + n)) * (2 * n + 1) / 6)
