1eaa920e79c8c447cab212c7e17027ae1146349f
Daniel Quintero <dgquintero02@hotmail.com>
2020-07-26 22:28:34 -0500

    17 answer

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
-from numpy.polynomial import polynomial as P
 """Calculates the integral of a polynomial"""
 
 
 def poly_integral(poly, C=0):
     """Calculates the integral of a polynomial"""
-    P.polyint(poly, C)
+    #return (P.polyint(poly, C))
     #return poli_int
+    if type(poly) is not list or len(poly) == 0:
+        return None
+    elif len(poly) == 1:
+        return [0]
+    elif poly == [0]:
+        return [C]
+    else:
+        integral_poly = [C]
+        [integral_poly.append(poly[i] / (i + 1)) for i in range(1, len(poly))]
+        #if integral_poly == 0:
+        #    return ([0])
+        return integral_poly
