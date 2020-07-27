7a62c662f3f11006128608dbb090bc6ce1608e74
Daniel Quintero <dgquintero02@hotmail.com>
2020-07-26 22:56:50 -0500

    17 answer

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -4,17 +4,18 @@
 
 def poly_integral(poly, C=0):
     """Calculates the integral of a polynomial"""
-    # return (P.polyint(poly, C))
-    # return poli_int
     if type(poly) is not list or len(poly) == 0:
         return None
     elif len(poly) == 1:
         return [0]
-    elif poly == [0]:
-        return [C]
+    #elif poly == [0]:
+        #return [C]
     else:
         integral_poly = [C]
-        [integral_poly.append(poly[i] / (i + 1)) for i in range(1, len(poly))]
-        # if integral_poly == 0:
-        #    return ([0])
+        for i in range(len(poly)):
+            x = poly[i] / (i + 1)
+            if x.is_integer():
+                integral_poly.append(int(x))
+            else:
+                integral_poly.append(x)
         return integral_poly
