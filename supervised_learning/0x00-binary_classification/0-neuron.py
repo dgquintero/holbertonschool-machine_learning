1c990538c2d0d3bf209e710bf409bfa8ae44d7f6
Daniel Quintero <dgquintero02@hotmail.com>
2020-08-03 13:03:40 -0500

    single neuron function added

---
+++ supervised_learning/0x00-binary_classification/0-neuron.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+class Neuron():
+    """ defines a single neuron performing binary classification
+    """
+
+    def __init__(self, nx):
+        """nx: number of inputs 
+        (weight, bias, Aactivate output) """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        
+        self.W = np.random.randn(nx).reshape(1, nx)
+        self.b = 0
+        self.A = 0
