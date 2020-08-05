4c96589f1d95f7f6d354ee9a32def380078a8bb5
Daniel Quintero <dgquintero02@hotmail.com>
2020-08-04 22:03:23 -0500

    change style on files

--- supervised_learning/0x00-binary_classification/1-neuron.py
+++ supervised_learning/0x00-binary_classification/1-neuron.py
@@ -8,13 +8,13 @@ class Neuron():
     """
 
     def __init__(self, nx):
-        """nx: number of inputs 
+        """nx: number of inputs
         (weight, bias, Aactivate output) """
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        
+
         self.__W = np.random.randn(nx).reshape(1, nx)
         self.__b = 0
         self.__A = 0
@@ -28,7 +28,7 @@ class Neuron():
     def b(self):
         """ getter function for bias """
         return self.__b
-    
+
     @property
     def A(self):
         """getter function for Activate output"""
