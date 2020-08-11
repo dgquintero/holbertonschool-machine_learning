d187aa5d8bd3543eea1b0c3af2f0a586038a24f4
Daniel Quintero <dgquintero02@hotmail.com>
2020-08-10 21:45:15 -0500

    pycode style added to 0file

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -7,11 +7,16 @@ def one_hot_encode(Y, classes):
     """
     Arguments:
         Y: numeric classes labels shape(m,)
-        classes: the maximum number of classes 
+        classes: the maximum number of classes
     Return: one-hot enconding Y shape(classes, m)
     """
+    if len(Y) == 0 or type(classes) is not int:
+        return None
+    elif not isinstance(Y, np.ndarray):
+        return None
+    elif classes <= np.argmax(Y):
+        return None
     # one hot encoder
     onehot_encoder = np.zeros((classes, len(Y)))
     onehot_encoder[Y, np.arange(len(Y))] = 1
     return onehot_encoder
-
