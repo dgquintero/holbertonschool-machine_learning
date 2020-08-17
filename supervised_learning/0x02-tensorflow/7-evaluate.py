bd62011071e352194e0841d4676f476f23480686
Daniel Quintero <dgquintero02@hotmail.com>
2020-08-16 23:59:55 -0500

    7 evaluate

---
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""evaluate function"""
+import tensorflow as tf
+
+
+def evaluate(X, Y, save_path):
+    """
+    evaluates the output of a neural network
+    Arguments:
+        X: the input data to evaluate
+        Y: the one-hot labels for X
+        save_path: location to load the model from
+    Returns: the network’s prediction, accuracy, and loss, respectively
+    """
+    pass
