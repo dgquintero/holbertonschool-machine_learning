2d78a813e3f5136b40eb81a9dd6c5d73a9088c66
Daniel Quintero <dgquintero02@hotmail.com>
2020-08-23 19:11:50 -0500

    momentum optimized function added

---
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""create_momentum_op function"""
+
+import tensorflow as tf
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """
+    
+    Arguments:
+        alpha: is the learning rate
+        beta1: is the momentum weight
+        loss: is the loss of the network
+    Returns: the momentum optimization operation
+    """
+    optimizer = tf.train.MomentumOptimizer(alpha, beta1)
+    optimizer.minimize(loss)
+    return optimizer
