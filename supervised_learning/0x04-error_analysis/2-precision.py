pycode fixed for precision func

supervised_learning/0x04-error_analysis/2-precision.py
@@ -18,5 +18,5 @@ def precision(confusion):
     false_pos = np.sum(confusion, axis=0) - true_pos
 
     precision = true_pos / (true_pos + false_pos)
-    # PPV = TP/(TP+FP)
+    # PPV = TP/(TP+FP)
     return precision