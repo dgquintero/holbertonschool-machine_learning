b8d6ee9f3d037009b37fd6d35448ceaf4a04b0d6
Daniel Quintero <dgquintero02@hotmail.com>
2020-08-28 18:46:53 -0500

    adding re_create_layer function

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -16,5 +16,24 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
         m: is the number of data points used
     Returns: the cost of the network accounting for L2 regularization
     """
-    
-
+    m = Y.shape[1]
+    W_copy = weights.copy()
+    for i in range(L, 0, -1):
+        if i == L - 1:
+            dZ = cache['A' + str(i + 1)] - Y
+            db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+            dW = (1 / m) * np.matmul(cache['A' + str(i - 1)], dZ.T).T
+            dW_L2 = dW + (lambtha / m) * W_copy['W' + str(i + 1)]
+        else:
+            dw2 = np.matmul(W_copy['W' + str(i)].T, dZ2)
+            tanh = 1 - (cache['A' + str(i + 1)] ** 2)
+            dZ = dw2 * tanh
+            db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+            dW = (1 / m) * np.matmul(dZ, cache['A' + str(i)].T)
+            dW_L2 = dW + (lambtha / m) * W_copy['W' + str(i + 1)]
+        # update weiths and bias
+        weights['W' + str(i+1)] = (W_copy['W'+str(i+1)] -\
+            (alpha * dW_L2)).T
+        weights['b' + str(i+1)] = W_copy['b'+str(i + 1)] -\
+            (alpha * db)
+        dZ2 = dZ
