1779bc4b477ec1ff761389ba831c7f0c6a814af6
Daniel Quintero <dgquintero02@hotmail.com>
2020-09-01 15:22:49 -0500

    style fixed for optimize function

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -5,7 +5,7 @@ import tensorflow.keras as K
 
 def optimize_model(network, alpha, beta1, beta2):
     """
-    Function that sets up Adam optimization 
+    Function that sets up Adam optimization
         Arguments:
             network: the model to optimize
             alpha: the learning rate
