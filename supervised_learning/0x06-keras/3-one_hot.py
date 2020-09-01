0b9a7985d94ce328bf57ef57234ea28f8b1c8edd
Daniel Quintero <dgquintero02@hotmail.com>
2020-09-01 16:00:25 -0500

    one_hot encode function

---
+++ supervised_learning/0x06-keras/3-one_hot.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""function optimize_model"""
+import tensorflow.keras as K
+
+
+def one_hot(labels, classes=None):
+    """
+    Function that converts a label vector into a one-hot matrix
+        Arguments:
+            labels
+            classes
+        Returns: the one-hot matrix
+    """
+    one_hot = tf.keras.utils.to_categorical(labels, num_classes=classes)
+    return one_hot
