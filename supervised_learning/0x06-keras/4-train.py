c12ffcd8f2f3c8568e3692ea44bd7f05cec125de
Daniel Quintero <dgquintero02@hotmail.com>
2020-09-01 16:45:13 -0500

    style error fixed

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -3,18 +3,20 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
-    """ 
+def train_model(network, data, labels, batch_size, epochs, verbose=True,
+                shuffle=False):
+    """
     Function that trains a model using mini-batch gradient descent
         Arguments:
             network: model to train
             data: contain the input data
             labels: containing the labels of data
-            batch_size: size of the batch used for mini-batch gradient descent
-            epochs: number of passes through data for mini-batch gradient descent
+            batch_size: size of the batch used for mini-batch gd
+            epochs: number of passes through data for mini-batch gd
             verbose: boolean if output should be printed during training
-            shuffle: boolean that determines whether to shuffle the batches every epoch
-        Returns: History object 
+            shuffle: boolean that determines whether to shuffle the batches
+                every epoch
+        Returns: History object
     """
     history = network.fit(data, labels, epochs=epochs,
                           batch_size=batch_size, verbose=verbose,
