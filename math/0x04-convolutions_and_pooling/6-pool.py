d9a4f50c930e3aa6daf8f6b86b4b5c2b236599d9
Daniel Quintero <dgquintero02@hotmail.com>
2020-09-07 23:01:12 -0500

    pooling function added

---
+++ math/0x04-convolutions_and_pooling/6-pool.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+""" convolve function"""
+import numpy as np
+
+
+def pool(images, kernel_shape, stride, mode='max'):
+    """that performs a convolution on images using multiple kernels
+    Arguments:
+        images: shape (m, h, w, c) containing multiple grayscale images
+            m: the number of images
+            h: the height in pixels of the images
+            w: the width in pixels of the images
+            c: is the number of channels in the image
+        kernel_shape: shape (kh, kw) containing the kernel shape for the pooling
+            kh: the height of the kernel
+            kw: the width of the kernel
+        stride:
+            sh: stride for the height of the image
+            sw: stride for the width of the image
+        mode: indicates the type of pooling
+            max: max pooling
+            avg: average pooling
+    Returns: ndarray containing the pooled images
+    """
+    m = images.shape[0]
+    h = images.shape[1]
+    w = images.shape[2]
+    c = images.shape[3]
+    kh = kernel_shape[0]
+    kw = kernel_shape[1]
+    sh = stride[0]
+    sw = stride[1]
+
+    if mode == 'max':
+        pooling = np.max
+    else: 
+        pooling = np.average
+
+    new_h = int(((h - kh) / sh) + 1)
+    new_w = int(((w - kh) / sw) + 1)
+
+    output_c = np.zeros((m, new_h, new_w, c))
+
+    for i in range(new_h):
+        for j in range(new_w):
+            x = i * sh
+            y = j * sw
+            output_c[:, i, j, :] = pooling(images[:, x:x+kh, y:y+kw, :],
+                                           axis=(1, 2))
+    return output_c
