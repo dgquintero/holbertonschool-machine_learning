2d86e1e10da3d3e69e89905b5ca5a5a4517d3196
Daniel Quintero <dgquintero02@hotmail.com>
2020-09-11 09:33:02 -0500

    letnet5 fixing function

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -17,56 +17,50 @@ def lenet5(x, y):
     """
     # Convolutional layer 1
     init = tf.contrib.layers.variance_scaling_initializer()
-    conv1 = tf.layers.Conv2d(
-        inputs=x,
+    conv1 = tf.layers.Conv2D(
         filters=6,   # Number of filters.
         kernel_size=5,    # Size of each filter is 5x5.
         padding="valid",     # No padding is applied to the input.
-        activation=tf.nn.relu
-        kernel_initializer=init)
+        activation=tf.nn.relu,
+        kernel_initializer=init)(x)
 
     # Pooling Layer #1
     # Sampling half the output of previous layer
     # Output: 14 * 14 * 6
     pool1 = tf.layers.MaxPooling2D(
-        inputs=conv1,
         pool_size=[2, 2],
-        strides=2)
+        strides=2)(conv1)
     # Convolutional Layer #2
     # Output: 10 * 10 * 16
     conv2 = tf.layers.Conv2D(
-        inputs=pool1,
         filters=16,
         kernel_size=5,
         padding="valid",
         activation=tf.nn.relu,
-        kernel_initializer=init
-    )
+        kernel_initializer=init)(pool1)
     # Pooling Layer #2
     # Output: 5 * 5 * 16
     pool2 = tf.layers.MaxPooling2D(
-        inputs=conv2,
         pool_size=[2, 2],
-        strides=2
-    )
+        strides=2)(conv2)
 
     # Reshaping output into a single dimention array for input
     # to fully connected layer
-    pool2_flat = tf.layers.Flatten(inputs=pool2)
+    pool2_flat = tf.layers.Flatten()(pool2)
 
     # Fully connected layer #1: Has 120 neurons
-    dense1 = tf.layers.Dense(inputs=pool2_flat,
-                             units=120,
-                             activation=tf.nn.relu
-                             kernel_initializer=init)
+    dense1 = tf.layers.Dense(
+        units=120,
+        activation=tf.nn.relu,
+        kernel_initializer=init)(pool2_flat)
     # Fully connected layer #2: Has 84 neurons
-    dense2 = tf.layers.Dense(inputs=dense1,
-                             units=84,
-                             activation=tf.nn.relu,
-                             kernel_initializer=init)
+    dense2 = tf.layers.Dense(
+        units=84,
+        activation=tf.nn.relu,
+        kernel_initializer=init)(dense1)
     # Output layer, 10 neurons for each digit
-    logits = tf.layers.Dense(inputs=dense2, units=10, kernel_initializer=init)
-
+    logits = tf.layers.Dense(units=10,
+                             kernel_initializer=init)(dense2)
     # prediction variable
     y_predict = logits
 
@@ -77,9 +71,9 @@ def lenet5(x, y):
     optimizer = tf.train.AdamOptimizer().minimize(loss)
 
     # For testing and prediction
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_pred, 1))
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_predict, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
-    y_pred = tf.nn.softmax(y_pred)
+    y_predict = tf.nn.softmax(y_predict)
 
-    return y_pred, optimizer, loss, accuracy
+    return y_predict, optimizer, loss, accuracy
