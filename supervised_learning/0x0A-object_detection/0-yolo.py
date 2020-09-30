6885afa34f5730760b6e93d31c5c4d0716301ca8
Daniel Quintero <dgquintero02@hotmail.com>
2020-09-30 17:08:55 -0500

    Yolo class added

---
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import tensorflow.keras as K
+
+class Yolo:
+    """Class Yolov3 to perform object detection"""
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """
+        class constructor
+        Arguments:
+            model_path: path to where a Darknet Keras model is stored
+            classes_path: path to where the list of class names used
+                for the Darknet model, listed in order of index, can be found
+            class_t: the box score threshold for the initial filtering step
+            nms_t: the IOU threshold for non-max suppression
+            anchors: containing all of the anchor boxes
+        Returns:
+        """
+        self.model = K.models.load_model(model_path)
+
+        with open(classes_path, 'r') as f:
+            self.class_names = [line.strip() for line in f]
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
